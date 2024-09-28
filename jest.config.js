549db16ed502f95b3ad9a6721ad1087102b80d7e
iam-truongtrungnghia <nghiatt@qasoftsolution.com>
2024-09-28 20:00:24 +0700

    setup: initialize app

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  },
+};
+
---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import '@/index.css';
+import App from '@/App';
+import store from '@/store.ts';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>
+);
